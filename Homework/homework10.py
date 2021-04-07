9d21631252ed2bda0f9b554e07115aca197598d0
Omega151 <hayaanrizvi@gmail.com>
2021-04-06 18:57:06 -0700

    Added Homework 10

---
+++ Homework/homework10.py
@@ -0,0 +1,38 @@
+# 1. Create a dictionary with the following key-value pair
+# and print the output on repl:
+
+# car, bmw
+# color, brown
+# number, 5
+
+dictionary1 = {
+    "car": "bmw",
+    "color": "brown",
+    "number": 5,
+}
+
+for key, value in dictionary1.items():
+    print(key, ":", value)
+
+
+# 2. For the dictionary above, delete the key-value pair {‘number’:5}
+
+del dictionary1["number"]
+
+# 3. Create a dictionary with the following key-value
+# pair and print the output it on repl
+
+# subjects, [‘math’,’english’]
+# students, {‘Name’: ‘Ahmad’,’Age’:5}
+
+dictionary2 = {
+    "subjects": ["math", "english"],
+    "students": {"Name": "Ahmad", "Age": 5},
+}
+
+for key, value in dictionary2.items():
+    print(key, ":", value)
+
+# For the dictionary above, add the key-value pair {‘word’:’hello’}
+
+dictionary2["word"] = "hello"
